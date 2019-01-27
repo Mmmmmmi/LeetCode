c35287376b8461f142a175d75de02bfd80e7ae8d
Mmmmmmi <812632545@qq.com>
2019-01-27 23:33:56 +0800

    完成----subsets

--- subsets.cpp
+++ subsets.cpp
@@ -40,8 +40,10 @@ public:
         return result;
     }
 private:
+    
     static void subsets(const vector<int> &S, vector<int> &path, size_t step,
                         vector<vector<int> > &result) {
+        /*
         if (step == S.size()) {
             result.push_back(path);
             cout << "cout << path: " << endl;
@@ -65,13 +67,45 @@ private:
         cout << endl;
         subsets(S, path, step + 1, result);
         path.pop_back();
+        */
+
+        //对于
+        //
+        if (step == S.size()) {
+            result.push_back(path);
+            return;
+        }
+
+        //每个元素只有两种选择
+        //选上 或者 选不上
+        //然后继续向下递归 
+        //等到回来的时候，将添加的再去掉 返回上一层 重新往下走
+        //如果是没有当前元素的话，就直接往下走
+        subsets(S, path, step + 1, result);
+        //如果是有元素的话就吧当前元素加进去
+        path.push_back(S[step]);
+        subsets(S, path, step + 1, result);
+        //返回的时候，再把添进去的元素取出来
+        path.pop_back();
+
+
+        //
     }
+    
 
 };
+
 int main()
 {
     vector<int> v = {1, 2};
     Solution s;
     vector<vector<int>> vret;
     vret = s.subsets(v);
+    for(auto e:vret) {
+        for (auto e1:e)
+            cout << e1 << "  ";
+        cout << endl;
+    }
+    return 0;
 }
+
