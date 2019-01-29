5217fb9a2358b9550d1810d6b83cbfd0d89f9cf0
Mmmmmmi <812632545@qq.com>
2019-01-30 00:31:23 +0800

    leetcode--121

--- makefile
+++ makefile
@@ -1,3 +1,3 @@
 
-maxDepth:maxDepth.cpp
+maxProfit:maxProfit.cpp
 	g++ -g -Wall -std=c++11  $^ -o $@
--- maxProfit.cpp
+++ maxProfit.cpp
@@ -33,13 +33,30 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         //动态规划问题，假设前一天都是收益最大的。就判断能不能在今天卖
+        size_t buy = 0;
+        size_t sell = 0;
         int maxprofix = 0;      //最大收益
-        for (int i = 0; i < prices.size(); i++) {
+        int profix = 0;         //收益
+        for (sell = 0; sell < prices.size(); sell++) {
+            //假设上一次都是收益最大的，因此 判断今天收益会不会更大，如果更大的话就更新收益的数据 否则就不变
+            profix = prices[sell] - prices[buy];
+            if (maxprofix < profix) {
+                maxprofix = profix;
+            }
 
+            //如果今天的价格是最低的，那就假设在今天买入
+            if (prices[sell] < prices[buy]) {
+                buy = sell;
+            }
         }
+        return maxprofix;
     }
+
 };
 int main()
 {
+    vector<int> prices{7, 6, 4, 3, 1};
+    Solution s;
+    cout << "maxprofit = " << s.maxProfit(prices) << endl;
     return 0;
 }
